2c388c0a702dfb3cc83b4faf114d500d59195195
Minseong Na <minseong.na@me.com>
2019-03-01 00:40:20 +0900

    코멘트 추가

--- problems/easy/No_7_e_reverseInteger.java
+++ problems/easy/No_7_e_reverseInteger.java
@@ -26,15 +26,21 @@ import java.math.BigDecimal;
  */
 public class No_7_e_reverseInteger {
     public static int reverseByChar(int x) {
+
+        // 숫자가 0이면 반전해도 0이기 때문에 0을 리턴한다.
         if( x == 0 ){
             return 0;
         }
 
         StringBuilder sb = new StringBuilder();
+
+        // 문자열로 처리할 땐 음수인지 판단하여 "-"를 먼저 더한다.
         if( x < 0 ){
             sb.append( "-" );
         }
 
+        // 숫자를 문자열로 바꾸고, 문자열의 마지막부터 sb에 더한다.
+        // 음수의 경우 마지막 순서에 "-"가 나오는데 break를 해서 sb.append를 피한다.
         char[] xCharArray = String.valueOf( x ).toCharArray();
         for( int ridx = xCharArray.length - 1 ; ridx >= 0; ridx-- ){
             char ch = xCharArray[ridx];
@@ -44,6 +50,7 @@ public class No_7_e_reverseInteger {
             sb.append( ch );
         }
 
+        // 파싱한 값이 정수범위를 넘어설 수 있으므로 long으로 계산하고 그 결과가 정수 범위인지 판한단다.
         long reverseX = new BigDecimal( sb.toString() ).longValue();
         if( reverseX > Integer.MAX_VALUE || reverseX < Integer.MIN_VALUE ){
             return 0;
@@ -54,15 +61,24 @@ public class No_7_e_reverseInteger {
     }
 
     public static int reverseByCalc( int x ){
+
+        // 숫자가 0이면 반전해도 0이기 때문에 0을 리턴한다.
         if( x == 0 ){
             return 0;
         }
+
+        // 나머지 연산자를 이용하여 x의 가장 낮은 자릿수 숫자를 구하고,
+        // 그 숫자를 reverseX에 더해주는데, 기존 reverseX를 10곱한 값에 더해준다.
+        // x는 1/10씩 작아지고 reverseX는 10배씩 커진다고 보면 된다.
         long reverseX = 0;
         while( x != 0 ){
             int mod = x % 10;
             x = x / 10;
             reverseX = ( reverseX * 10 ) + mod;
         }
+
+        // 자바의 경우 integer는 32비트 고정이라 이런 식이 필요 없지만,
+        // Integer.MAX_VALUE, Integer.MIN_VALUE가 64비트값이라면 이런식으로 처리해주면 된다.
         double MIN_32BIT_INT = Math.pow( -2, 31 );
         double MAX_32BIT_INT = Math.pow(  2, 31 ) - 1;
         if( reverseX < MIN_32BIT_INT || MAX_32BIT_INT < reverseX ){
